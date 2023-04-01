be881ef445e022573d06323a5ac619684f1f3151
Sabyr Shatekov <shatekov.sabyr@gmail.com>
2023-04-01 20:05:12 +0600

    Learned: Swagger and Redoc

--- service/model.py
+++ service/model.py
@@ -4,3 +4,11 @@ from pydantic import BaseModel
 class Todo(BaseModel):
     id: int
     item: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "id": 1,
+                "item": "Example schema!"
+            }
+        }
